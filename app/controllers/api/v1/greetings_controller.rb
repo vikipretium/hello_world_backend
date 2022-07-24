d03464690a248241d2bb8372af129b7409bd1a4e
vikipretium <vighneshpasupathy@gmail.com>
2022-07-24 16:33:57 +0530

    fix greeting conrtroller naming convention

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Greeting < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
     def index
         random_greetings = Greeting.all.sample
         render json: random_greetings
